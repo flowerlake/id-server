1576262b238f5062cb4f65146220f3cbb5a31bee
flowerlake <cherryg963@gmail.com>
2022-05-19 21:14:16 +0800

    Changes

---
+++ src/main/java/cn/felord/idserver/entity/PRTest.java
@@ -0,0 +1,12 @@
+package cn.felord.idserver.entity;
+
+public class PRTest {
+
+    private int id;
+    private String prName;
+
+    public PRTest(int id, String prName) {
+        this.id = id;
+        this.prName = prName;
+    }
+}
